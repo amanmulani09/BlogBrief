8f26d35937b657a8ffe23c26b2aa3b79e1b64614
amanmulani09 <mulaniaman0504@gmail.com>
2023-05-31 23:44:56 +0530

    build the demo component for user input

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,6 +14,9 @@ const Hero = () => {
       </nav>
       <h1 className='head_text'> Summarize Articles with <br className='max-md:hidden'/>
       <span className='orange_gradient'>OpeanAI GPT-4</span> </h1>
+      <h2 className='desc'>
+        simplify your reading with BlogBrief. an Open-source article summarizer that transforms lengthy articles into clear and concise summaries
+      </h2>
     </header>
   )
 }
